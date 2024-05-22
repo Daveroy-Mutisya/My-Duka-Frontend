fc3f56c63866c03a1c3b3a2476bf57c18fedb4bc
Ronnie <ronnielangat2020@gmail.com>
2024-05-23 02:48:55 +0300

    styled the Dashboard for merchant

--- src/pages/merchant/components/MerchantSideBar.jsx
+++ src/pages/merchant/components/MerchantSideBar.jsx
@@ -5,16 +5,16 @@ import "./MerchantSideBar.css";
 function MerchantSideBar() {
     return (
         <div className="sidebar">
-            <h2 className="sidebar-title">Merchant Dashboard</h2>
+            <h2 className="sidebar-title">Dashboard</h2>
             <ul className="sidebar-menu">
                 <li className="sidebar-item">
-                    <Link to="/invite-admin" className="sidebar-link">Invite Admin</Link>
+                    <Link to="/View-performance" className="sidebar-link">View Perfomance</Link>
                 </li>
                 <li className="sidebar-item">
                     <Link to="/store" className="sidebar-link">Store</Link>
                 </li>
                 <li className="sidebar-item">
-                    <Link to="/view-performance" className="sidebar-link">View Performance</Link>
+                    <Link to="/invite-admin" className="sidebar-link">Invite admin</Link>
                 </li>
                 <li className="sidebar-item">
                     <Link to="/admin-management" className="sidebar-link">Admin Management</Link>
